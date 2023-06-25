9cf3e8941fcdb68e260c5aafcce7257a88cb1a76
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-06-25 11:10:02 +0530

    Add ice cube

--- src/main/kotlin/dev/shreyasm/stuff/Stuff.kt
+++ src/main/kotlin/dev/shreyasm/stuff/Stuff.kt
@@ -1,10 +1,22 @@
 package dev.shreyasm.stuff
 
+import dev.shreyasm.stuff.items.IceCube
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
 import org.slf4j.LoggerFactory
+
 
 object Stuff : ModInitializer {
   private val logger = LoggerFactory.getLogger("stuff")
+
+  val ICE_CUBE = Registry.register(
+    Registries.ITEM,
+    Identifier("stuff", "ice_cube"),
+    IceCube(FabricItemSettings()),
+  )
 
   override fun onInitialize() {
     // This code runs as soon as Minecraft is in a mod-load-ready state.
---
+++ src/main/kotlin/dev/shreyasm/stuff/items/IceCube.kt
@@ -0,0 +1,5 @@
+package dev.shreyasm.stuff.items
+
+import net.minecraft.item.Item
+
+class IceCube(settings: Settings?) : Item(settings)
