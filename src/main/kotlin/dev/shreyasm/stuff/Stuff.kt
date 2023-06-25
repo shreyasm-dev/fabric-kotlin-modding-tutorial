c2e54ae96116814e11676ab3a06e9bb88131cf97
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-06-25 11:43:12 +0530

    Add properties to ice cubes

--- src/main/kotlin/dev/shreyasm/stuff/Stuff.kt
+++ src/main/kotlin/dev/shreyasm/stuff/Stuff.kt
@@ -3,8 +3,12 @@ package dev.shreyasm.stuff
 import dev.shreyasm.stuff.items.IceCube
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup
+import net.minecraft.item.ItemGroup
+import net.minecraft.item.ItemStack
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
+import net.minecraft.text.Text
 import net.minecraft.util.Identifier
 import org.slf4j.LoggerFactory
 
@@ -15,7 +19,7 @@ object Stuff : ModInitializer {
   val ICE_CUBE = Registry.register(
     Registries.ITEM,
     Identifier("stuff", "ice_cube"),
-    IceCube(FabricItemSettings()),
+    IceCube(FabricItemSettings().maxCount(16).fireproof()),
   )
 
   override fun onInitialize() {
