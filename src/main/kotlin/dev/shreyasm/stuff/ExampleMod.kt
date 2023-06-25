0c2adab2dc6dcfce7f28ff42921b1ec878584d51
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-06-25 10:29:49 +0530

    Initialize project

---
+++ src/main/kotlin/dev/shreyasm/stuff/ExampleMod.kt
@@ -0,0 +1,15 @@
+package dev.shreyasm.stuff
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object ExampleMod : ModInitializer {
+    private val logger = LoggerFactory.getLogger("stuff")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
---
+++ src/main/kotlin/dev/shreyasm/stuff/ExampleModDataGenerator.kt
@@ -0,0 +1,9 @@
+package dev.shreyasm.stuff
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object ExampleModDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
